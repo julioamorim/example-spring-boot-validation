01b2d7190b06d226143b942990e8ce6402e6243a
Julio Amorim <julio.l.amorim00@gmail.com>
2020-06-03 12:05:50 -0300

    Classe user and lombok dependency added

---
+++ src/main/java/io/github/juioamorim/demo/User.java
@@ -0,0 +1,12 @@
+package io.github.juioamorim.demo;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+	private int userId;
+	private String userName;
+	private String userEmail;
+
+}
