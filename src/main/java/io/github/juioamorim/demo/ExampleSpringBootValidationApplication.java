0d2072871188370224db9a5c20fd111e6298b226
Julio Amorim <julio.l.amorim00@gmail.com>
2020-06-04 00:30:12 -0300

    Removed unused dependencies from pom, adjust Nameclature of the project

--- src/main/java/io/github/juioamorim/demo/ExampleSpringBootValidationApplication.java
+++ src/main/java/io/github/juioamorim/demo/ExampleSpringBootValidationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ExampleSpringBootValidationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ExampleSpringBootValidationApplication.class, args);
 	}
 
 }
