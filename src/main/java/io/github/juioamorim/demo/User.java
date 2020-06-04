d624396bcacc008669dc637a665eff15a8aba4ec
Julio Amorim <julio.l.amorim00@gmail.com>
2020-06-03 22:57:16 -0300

    Lombok annotations added, id, generatedValue config to id attribute added and validation NotBlank added to the user and email fields

--- src/main/java/io/github/juioamorim/demo/User.java
+++ src/main/java/io/github/juioamorim/demo/User.java
@@ -1,12 +1,37 @@
 package io.github.juioamorim.demo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class User {
 
+	@Id
+	/*
+	 * Indicates that the persistence provider should pick an appropriate strategy
+	 * for the particular database. The AUTO generation strategy may expect a
+	 * database resource to exist, or it may attempt to create one. A vendor may
+	 * provide documentation on how to create such resources in the event that it
+	 * does not support schema generation or cannot create the schema resource at
+	 * runtime.
+	 */
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userId;
+
+	@NotBlank(message = "Username is required")
 	private String userName;
+
+	@NotBlank(message = "Email is reqeuired")
 	private String userEmail;
 
 }
