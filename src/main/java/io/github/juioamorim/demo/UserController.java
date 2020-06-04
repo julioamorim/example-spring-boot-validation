fe09d989a7c6ae399970116b0d8c6175d30caf8d
Julio Amorim <julio.l.amorim00@gmail.com>
2020-06-03 23:21:59 -0300

    Handle to get exception handle added to capture exceptions

--- src/main/java/io/github/juioamorim/demo/UserController.java
+++ src/main/java/io/github/juioamorim/demo/UserController.java
@@ -1,14 +1,26 @@
 package io.github.juioamorim.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+	@Autowired
+	UserRepository userRepository;
 
 	// in the context for the validation, the annotation @valid is the most
 	// important annotation
@@ -18,7 +30,20 @@ public class UserController {
 	ResponseEntity<String> addUser(@Valid @RequestBody User user) {
 
 		// persisting user
+		userRepository.save(user);
 		return ResponseEntity.ok("User is valid");
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
 	}
 
 }
